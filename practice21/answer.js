adc523b91fb9e27e7c1f2c7fbd9ea0ad972e7cc7
ltakiguc <ltakiguc@uci.edu>
2016-10-20 18:17:11 -0700

    Bipolar for loop completed

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,12 @@
-answer.js
+/*
+Project Name: Practice Problems - Practice 20
+File Name: index.html
+Author: Lance Takiguchi
+Date: 10/19/2016 Time: 14:34 
+Objective: Create a single loop that simultaneously counts up on one variable, while counting down on another. The only code inside the for loop should be a console.log that lists both variables.
+Prompt: https://github.com/Learning-Fuze/practice-problems/blob/master/practice21/README.md
+JS Bin: http://jsbin.com/xatazepeso/1/edit?js,console
+*/
+for(var i = 1, j = 10; i < 11 || j > 0; i++, j--){
+	console.log("i: " + i + " & j: " + j);
+}
