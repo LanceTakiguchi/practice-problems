0ed18705341f0e67990c88dfc8990c8e9485ef8c
ltakiguc <ltakiguc@uci.edu>
2016-11-05 15:22:35 -0700

    Init file setup

--- practice22/answer.js
+++ practice22/answer.js
@@ -1,1 +1,9 @@
-answer.js
+/*
+Project Name: Sort by Field
+File Name: answer.js
+Author: Lance Takiguchi
+Date: 11/05/2016 Time: 15:21 
+Objective: Sort an Array of Objects by the keys given a key
+Prompt:  https://github.com/Learning-Fuze/practice-problems/blob/master/practice22/README.md
+*/
+
