a5da2e68b81e5862a5c0f7c9266efcba884b39bd
ltakiguc <ltakiguc@uci.edu>
2016-11-07 10:23:50 -0800

    function completed. One test completed. Also saving practice22 for unknown reason

--- practice22/answer.js
+++ practice22/answer.js
@@ -5,6 +5,7 @@ Author: Lance Takiguchi
 Date: 11/05/2016 Time: 15:21 
 Objective: Sort an Array of Objects by the keys given a key
 Prompt:  https://github.com/Learning-Fuze/practice-problems/blob/master/practice22/README.md
+JSBin: http://jsbin.com/codiqitawu/edit?js,console
 */
 /**
  * Takes an Array of objects and a search key and sorts the array by the a key in the objects. Uses bubble sort
