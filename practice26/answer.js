ffc289b5dfaf10049207b308879137712e813c44
ltakiguc <ltakiguc@uci.edu>
2016-11-08 11:34:27 -0800

    Comments added

--- practice26/answer.js
+++ practice26/answer.js
@@ -3,16 +3,23 @@ Project Name: Practice 26 - Search Array
 File Name: answer.js
 Author: Lance Takiguchi
 Date: 11/08/2016 Time: 11:23 
-Objective: 
-Prompt: 
+Objective: Look for any keys in a target Array
+Prompt: https://github.com/LanceTakiguchi/practice-problems/blob/master/practice26/README.md
 */
 
+/**
+ * Given an Array of keys to look for in a hay Array
+ * @param  [Strings] hay    An Array of Strings that is the target of the search
+ * @param  [Strings] needle An Array of Strings that is the keys to be looking for in the target
+ * @return [Strings]        An Array of Strings where the a needle was found in the hay
+ */
 search_array = function(hay, needle){
 	var output = [];
 	for(var n in needle){
 		for(var h in hay){
 			if(hay[h] === needle[n]){
 				output.push(needle[n])
+				// Match has been noted, no need to further search for this particular key
 				break;
 			}
 		}
