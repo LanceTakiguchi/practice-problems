fc6d42aa53d8beb37348d95ea608cd6ab06a599e
ltakiguc <ltakiguc@uci.edu>
2016-11-11 15:04:51 -0800

    File Setup

---
+++ practice08/script.js
@@ -0,0 +1,10 @@
+/*
+Project Name: Practice 08 - Char Convert
+File Name: script.js
+Author: Lance Takiguchi
+Date: 11/11/2016 Time: 15:01 
+Objective: Convert characters in a string into ASCII numbers
+Prompt: https://github.com/LanceTakiguchi/practice-problems/blob/master/practice08/README.md
+Level: 3 - 1
+JS Bin: 
+*/
